fc052fc68680d30a96960d5ae7caf6c4c3b23a4f
matthewgraham1 <mjg267106@gmail.com>
2021-04-04 11:13:09 -0400

    Add alias setting and expansion

--- AliasTable.cpp
+++ AliasTable.cpp
@@ -7,6 +7,19 @@ AliasTable& AliasTable::the()
     return s_alias_table;
 }
 
+void AliasTable::set(const std::string& alias, const std::string& word)
+{
+    if (alias == word) {
+        fprintf(stderr, "Attempted to make %s an alias of itself\n", alias.c_str());
+        return;
+    }
+    if (auto result = m_table.find(word); result != m_table.end() && result->second == alias) {
+        fprintf(stderr, "Attempted to make %s an alias of %s, which would make an infinite loop!\n", alias.c_str(), word.c_str());
+        return;
+    }
+    m_table.insert({ alias, word });
+}
+
 std::string AliasTable::get(const std::string& alias) const
 {
     auto result = m_table.find(alias);
--- AliasTable.h
+++ AliasTable.h
@@ -6,5 +6,6 @@
 class AliasTable : public VariableTable {
 public:
     static AliasTable& the();
+    void set(const std::string&, const std::string&);
     virtual std::string get(const std::string&) const override;
 };
